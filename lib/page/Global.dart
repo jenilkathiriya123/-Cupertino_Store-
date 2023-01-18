97e49174cf4266c76fb8a198d4413eee54dad4c8
jenil kathiriya <6783jenilkathiriya@gmail.com>
2023-01-18 15:38:46 +0530

    first update

---
+++ lib/page/Global.dart
@@ -0,0 +1,102 @@
+class Global {
+  static List<Map<String, dynamic>> Productlist = [
+    {
+      'title': "Vagabond sack",
+      'images': "assets/images/1.jpeg",
+      'price': "\$ 45",
+      'price2': "45",
+    },
+    {
+      'title': "Stella sunglasses",
+      'images': "assets/images/2.jpeg",
+      'price': "\$ 58",
+      'price2': "58",
+    },
+    {
+      'title': "Whitney belt",
+      'images': "assets/images/3.jpeg",
+      'price': "\$ 35",
+      'price2': "35",
+    },
+    {
+      'title': "Garden strand",
+      'images': "assets/images/4.jpeg",
+      'price': "\$ 98",
+      'price2': "98",
+    },
+    {
+      'title': "Strut earrings",
+      'images': "assets/images/5.jpeg",
+      'price': "\$ 34",
+      'price2': "34",
+    },
+    {
+      'title': "Varsity socks",
+      'images': "assets/images/6.jpeg",
+      'price': "\$ 12",
+      'price2': "12",
+    },
+    {
+      'title': "Weave keyring",
+      'images': "assets/images/7.jpeg",
+      'price': "\$ 16",
+      'price2': "16",
+    },
+    {
+      'title': "Shirt",
+      'images': "assets/images/8.jpeg",
+      'price': "\$ 110",
+      'price2': "110",
+    },
+    {
+      'title': "Ring",
+      'images': "assets/images/9.jpeg",
+      'price': "\$ 80",
+      'price2': "80",
+    },
+    {
+      'title': "Bracelet",
+      'images': "assets/images/10.jpeg",
+      'price': "\$ 40",
+      'price2': "40",
+    },
+  ];
+  static List<Map<String, dynamic>> Search = [
+    {
+      'title': "Chambray shirt",
+      'images': "assets/images/Chambray shirt.jpeg",
+      'price': "\$ 70",
+      'price2': "70",
+    },
+    {
+      'title': "White pinstripe shirt",
+      'images': "assets/images/White pinstripe shirt.jpeg",
+      'price': "\$ 75",
+      'price2': "75",
+    },
+    {
+      'title': "Surf and perf shirt",
+      'images': "assets/images/Surf and perf shirt.jpeg",
+      'price': "\$ 48",
+      'price2': "48",
+    },
+    {
+      'title': "Sunshirt dress",
+      'images': "assets/images/sunshirt dress.jpeg",
+      'price': "\$ 58",
+      'price2': "58",
+    },
+    {
+      'title': "Chambray shirt",
+      'images': "assets/images/Chambrayshirt.jpeg",
+      'price': "\$ 38",
+      'price2': "38",
+    },
+   ];
+  static List ItemList = [] ;
+  static String? name;
+  static String? image;
+  static String? price;
+  static String? price2;
+  static int total=0;
+}
